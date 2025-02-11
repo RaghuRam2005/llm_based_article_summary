3e474ad96f1adfbdcca9cb3b9e975e5a8f74ecf1
atom21 <lingutlaraghuramsai@gmail.com>
2025-02-11 15:38:26 +0530

    update chat structure

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -3,8 +3,16 @@ import requests
 
 st.title("LLM based article summary")
 
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message['role']):
+        st.markdown(message['content'])
+
 # Input for user query
-query = st.chat_input("Enter the article name you want to search: ")
+query = st.chat_input("what do you want?")
 
 if len(str(query)) > 30:
     st.warning("Query length limit exceeded!")
@@ -12,17 +20,18 @@ if len(str(query)) > 30:
 if query:
     # Make a POST request to the Flask API
     with st.chat_message("user"):
-        st.write(query)
-    st.session_state.messages.append({"role": "user", "content": query})
+        st.markdown(query)
+        st.session_state.messages.append({"role":"user", 'content':query})
+    
     flask_url = "http://localhost:5001/query"
     response = requests.post(flask_url, json={"query": query})
 
     if response.status_code == 200:
         with st.chat_message("assistant"):
             answer = response.json().get('response', "No response received.")
-            st.write("Answer:", answer)
+            st.session_state.messages.append({"role":'assistant', 'content':answer})
+            st.markdown(answer)
     else:
         with st.chat_message("assistant"):
-            print(response.content)
-            st.write(f"Error: {response.status_code}")
-    st.session_state.messages.append({'role':"assistant", "content":response})
+            st.warning(f"{response.status_code}")
+            st.session_state.messages.append({'role':'assistant', 'content':response.status_code})
